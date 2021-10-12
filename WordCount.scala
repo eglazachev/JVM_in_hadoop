b1741c7b44346073ed9e8846d12662b6af98d1f9
eglazachev <63977872+eglazachev@users.noreply.github.com>
2021-10-13 01:30:34 +0300

    code for HW3 of JVM in hadoop

--- WordCount.scala
+++ WordCount.scala
@@ -1,17 +1,22 @@
-import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object WordCount {
   def main(args: Array[String]): Unit = {
     val input = args(0)
     val output = args(1)
-    val sc = new SparkContext()
-    sc.textFile(input)
-      .flatMap(line => line.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase().split("\\s"))
-      .map(word => (word, 1))
-      .reduceByKey(_ + _) // .reduceByKey((x, y) => x + y)
-      .sortBy { case (word, count) => count } // .sortBy(_._2) or .sortBy(pair => pair._2)
-      .map(_.swap)
-      .saveAsTextFile(output)
-    sc.stop()
+    val stopWords = Seq("di", "yg")
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.sqlContext.implicits._
+    spark.read.option("header", "true").csv(input)
+      .select(concat_ws(" ", $"class", $"comment") as "docs")
+      .select(lower($"docs") as "docs")
+      .select(regexp_replace($"docs","[^a-z0-9_ ]","") as "docs")
+      .select(split($"docs",
+        "\\s") as "words")
+      .select(explode($"words") as "word").filter(!$"word".isin(stopWords:_*))
+      .groupBy($"word").count().sort("count")
+      .select($"count", $"word").write.mode("overwrite").csv(output)
+    spark.stop()
   }
 }
